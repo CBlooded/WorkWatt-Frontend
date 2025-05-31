c9e4ef5a2d4f6d632a4ea722b8d8e09fc941f873
SharpP03 <Patrykstoncel186@wp.pl>
2025-05-31 16:19:18 +0200

    NavBar basic

--- src/components/navbar.tsx
+++ src/components/navbar.tsx
@@ -3,25 +3,32 @@ import InfoOutlineIcon from "@mui/icons-material/InfoOutline";
 import CallIcon from "@mui/icons-material/Call";
 import HomeIcon from "@mui/icons-material/Home";
 
+import "@fontsource/roboto/300.css";
+import "@fontsource/roboto/400.css";
+import "@fontsource/roboto/500.css";
+import "@fontsource/roboto/700.css";
 
 function navbar() {
   return (
     <>
-      <div className="main">
-        <div className="logo">
-<img src="/src/resources/logo.png" alt="Workwatt" />
-        </div>
-        <div className="link">
-          <HomeIcon />
-          <a href="/">Home</a>
-        </div>
-        <div className="link">
-          <InfoOutlineIcon />
-          <a href="/About">About</a>
-        </div>
-        <div className="link">
-          <CallIcon />
-          <a href="/Contact">Contact</a>
+      <div className="slider">
+        <div className="sidebar">
+          <div className="title">WorkWatt</div>
+          <div className="logo">
+            {/* <img src="/src/resources/logo.png" alt="Workwatt" /> */}
+          </div>
+          <div className="link">
+            <HomeIcon />
+            <a href="/">Home</a>
+          </div>
+          <div className="link">
+            <InfoOutlineIcon />
+            <a href="/About">About</a>
+          </div>
+          <div className="link">
+            <CallIcon />
+            <a href="/Contact">Contact</a>
+          </div>
         </div>
       </div>
     </>
