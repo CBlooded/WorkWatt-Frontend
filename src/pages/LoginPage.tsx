7c6ff659a1c01d414e6bc53019af170094adafae
Ereffe <eriffe9@gmail.com>
2025-05-31 18:20:12 +0200

    feat: add LoginPage component and update routing in App component

---
+++ src/pages/LoginPage.tsx
@@ -0,0 +1,13 @@
+import React from "react";
+import { Box } from "@mui/material";
+import LoginForm from "../components/LoginForm";
+
+const LoginPage = () => {
+  return (
+    <Box>
+      <LoginForm />
+    </Box>
+  );
+};
+
+export default LoginPage;
