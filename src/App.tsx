c73c502968a4948fd33dfc809445f5975ed84380
Ereffe <eriffe9@gmail.com>
2025-05-31 17:06:47 +0200

    feat: implement routing with ProtectedRoute and update App component structure

--- src/App.tsx
+++ src/App.tsx
@@ -4,12 +4,21 @@ import "@fontsource/roboto/400.css";
 import "@fontsource/roboto/500.css";
 import "@fontsource/roboto/700.css";
 import LoginForm from "./components/LoginForm";
+import { Route, Routes } from "react-router";
+import ProtectedRoute from "./components/ProtectedRoute";
 
 function App() {
   return (
     <>
       <div className="app">
-        <LoginForm></LoginForm>
+        <Routes>
+          <Route path="/" element={<LoginForm />} />
+          <ProtectedRoute>
+            <Route path="/tempRegister" />
+            <Route path="/register" />
+            <Route path="/dashboard" />
+          </ProtectedRoute>
+        </Routes>
       </div>
     </>
   );
