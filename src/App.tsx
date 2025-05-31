40d93f8c867a1c67fad7617d3e806f86e6197617
SharpP03 <Patrykstoncel186@wp.pl>
2025-05-31 17:06:52 +0200

    RegisterForm - main functions.

--- src/App.tsx
+++ src/App.tsx
@@ -3,13 +3,15 @@ import "@fontsource/roboto/300.css";
 import "@fontsource/roboto/400.css";
 import "@fontsource/roboto/500.css";
 import "@fontsource/roboto/700.css";
-import LoginForm from "./components/LoginForm";
+// import LoginForm from "./components/LoginForm";
+import RegisterForm from "./components/RegisterForm";
 
 function App() {
   return (
     <>
       <div className="app">
-        <LoginForm></LoginForm>
+        {/* <LoginForm></LoginForm> */}
+        <RegisterForm></RegisterForm>
       </div>
     </>
   );
