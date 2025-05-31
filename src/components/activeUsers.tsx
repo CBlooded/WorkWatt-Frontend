91e33408c1b4788001ba44140aa450eba76231ba
SharpP03 <Patrykstoncel186@wp.pl>
2025-05-31 21:53:46 +0200

    minor example fixes.

--- src/components/activeUsers.tsx
+++ src/components/activeUsers.tsx
@@ -2,45 +2,51 @@ import { Paper, List, ListItem, Box, Typography } from "@mui/material";
 
 function ActiveUsers() {
   const users = [
-    { id: "user1", time: "10:00 AM" },
-    { id: "user2", time: "11:30 AM" },
-    { id: "user3", time: "1:15 PM" },
+    { id: "user1", time: "21 hours 25 minutes" },
+    { id: "user2", time: "21 hours 43 minutes" },
+    { id: "user3", time: "43 hours 12 minutes" },
   ];
 
   return (
-    <>
-      <Paper
-        style={{ maxHeight: 200, width: 500, overflow: "auto", padding: 8 }}
-      >
-        <List>
-          <ListItem key={"label"} disableGutters>
+    <Paper
+      style={{
+        maxHeight: "30vh", // 30% of viewport height
+        width: "80vw", // 80% of viewport width
+        overflow: "auto",
+        padding: "2vw", // Responsive padding
+        margin: "auto", // Center horizontally
+      }}
+    >
+      <List>
+        {/* labels */}
+        <ListItem key={"label"} disableGutters>
+          <Box
+            display="flex"
+            justifyContent="space-between"
+            width="100%"
+            px="1vw"
+          >
+            <Typography fontWeight="bold">User ID</Typography>
+            <Typography color="text.secondary">Uptime</Typography>
+          </Box>
+        </ListItem>
+
+        {/* values */}
+        {users.map((user, index) => (
+          <ListItem key={index} disableGutters>
             <Box
               display="flex"
               justifyContent="space-between"
               width="100%"
-              px={1}
+              px="1vw"
             >
-              <Typography fontWeight="bold">User ID</Typography>
-              <Typography color="text.secondary">Time</Typography>
+              <Typography fontWeight="bold">ID: {user.id}</Typography>
+              <Typography color="text.secondary">{user.time}</Typography>
             </Box>
           </ListItem>
-
-          {users.map((user, index) => (
-            <ListItem key={index} disableGutters>
-              <Box
-                display="flex"
-                justifyContent="space-between"
-                width="100%"
-                px={1}
-              >
-                <Typography fontWeight="bold">ID: {user.id}</Typography>
-                <Typography color="text.secondary">{user.time}</Typography>
-              </Box>
-            </ListItem>
-          ))}
-        </List>
-      </Paper>
-    </>
+        ))}
+      </List>
+    </Paper>
   );
 }
 
