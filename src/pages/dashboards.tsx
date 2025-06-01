ac036dc8e98fe92902d9dc721040b924581e3bd6
SharpP03 <Patrykstoncel186@wp.pl>
2025-06-01 02:11:51 +0200

    structure and placeholders for charts and data.

--- src/pages/dashboards.tsx
+++ src/pages/dashboards.tsx
@@ -13,19 +13,28 @@ const Dashboard = () => {
         sx={{
           padding: 2,
           maxWidth: "70vw",
-          margin: "auto",
+          marginTop: "max(100vh, 550px)",
+        //   margin: "auto",
         }}
       >
         <Typography variant="h4" fontWeight="bold">
           Dashboard
         </Typography>
-        <div style={{ display: "flex", justifyContent: "space-between", flexWrap: "wrap",gap: "4vh"}}>
+        <div
+          style={{
+            // backgroundColor: "red",
+            display: "flex",
+            justifyContent: "space-between",
+            flexWrap: "wrap",
+            gap: "4vh",
+          }}
+        >
           {/* Active Users */}
           <ActiveUsers />
 
           {/* System Summary */}
-          <Paper elevation={3} sx={{ p: 2 , flex: 3}}>
-            <Typography variant="h6">System Summary</Typography>
+          <Paper elevation={3} sx={{ p: 2, flex: 3, borderRadius: "20px" }}>
+            <Typography variant="h6">Usage ranking</Typography>
             <Typography color="text.secondary">
               Energy used today: 123.5 kWh
             </Typography>
@@ -33,16 +42,30 @@ const Dashboard = () => {
               Total users online: 3
             </Typography>
           </Paper>
-          <Paper elevation={3} sx={{ p: 2, flex: 1 }}>
-            <Typography variant="h6">System Summary</Typography>
+          <Paper elevation={3} sx={{ p: 2, flex: 1, borderRadius: "20px" }}>
+            <Typography variant="h6">Usage summary</Typography>
             <Typography color="text.secondary">
               Energy used today: 123.5 kWh
             </Typography>
             <Typography color="text.secondary">
-              Total users online: 3
+              Average month usage: 123.5 kWh
             </Typography>
           </Paper>
         </div>
+        <Paper
+          elevation={3}
+          sx={{ p: 2, borderRadius: "20px", height: "50vh" }}
+        >
+          <Typography variant="h6">Insert Chart here</Typography>
+          <div>{/* TODO: ADD CHART */}</div>
+        </Paper>
+        <Paper
+          elevation={3}
+          sx={{ p: 2, borderRadius: "20px", height: "50vh" }}
+        >
+          <Typography variant="h6">Cost comparde with solar panels</Typography>
+          <div>{/* TODO: ADD savings chart */}</div>
+        </Paper>
       </Box>
     </Navbar>
   );
