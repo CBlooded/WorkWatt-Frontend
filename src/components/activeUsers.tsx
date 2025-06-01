4459d5f0f82ad261f824be9e0875b14a3ffa2e68
SharpP03 <Patrykstoncel186@wp.pl>
2025-06-01 10:00:16 +0200

    minor changes

--- src/components/activeUsers.tsx
+++ src/components/activeUsers.tsx
@@ -1,7 +1,54 @@
+
 import { Paper, List, ListItem, Box, Typography } from "@mui/material";
+import { Client } from "@stomp/stompjs";
+import { useEffect, useState } from "react";
+import SockJS from "sockjs-client/dist/sockjs"
+
+
+
 
 function ActiveUsers() {
-  const users = [
+  const [users, setUsers] = useState<{ id: string; time: string }[]>([]);
+
+  useEffect(() => {
+    const client = new Client({
+      webSocketFactory: () => new SockJS("http://localhost:8080/ws"), // backend WS endpoint
+      reconnectDelay: 5000,
+      onConnect: () => {
+        console.log(" Connected to WebSocket");
+
+        client.subscribe("/topic/active-users", (message) => {
+
+          if (message.body) {
+            try {
+              const data = JSON.parse(message.body);
+              if (Array.isArray(data)) {
+                setUsers(data);
+              } else {
+                console.warn("Received non-array WebSocket data:", data);
+              }
+            } catch (err) {
+              console.error(" Failed to parse WebSocket message:", err);
+            }
+          }
+        });
+      },
+      onStompError: (frame) => {
+        console.error("STOMP error:", frame);
+      },
+    });
+
+    client.activate();
+
+    return () => {
+      client.deactivate();
+    };
+  }, []);
+
+
+  
+
+  const usersSample = [
     { id: "user1", time: "21 hours 25 minutes" },
     { id: "user2", time: "21 hours 43 minutes" },
     { id: "user3", time: "43 hours 12 minutes" },
